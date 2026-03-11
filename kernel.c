456a2dfdac6261fdd711fb242d121d04eb498978
Titzsx <119414917+lorenzo203@users.noreply.github.com>
2026-03-11 12:50:40 +0100

    kernel panic implementation

--- kernel.c
+++ kernel.c
@@ -44,6 +44,8 @@ void putchar(char ch) {
 }
 
 void kernel_main(void) {
+    memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
+    PANIC("Panic Test!");
     printf("\n\nHello%s\n","World!");
     printf("1 + 2 = %d, %x\n", 1+2, 0x1234abcd);
     for (;;) {
